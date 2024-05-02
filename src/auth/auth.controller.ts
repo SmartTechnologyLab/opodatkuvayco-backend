f025d686ee02c9ca6fbaf17b7b0d0d99ca5d4305
Yevhenii <zheki2004@gmail.com>
2024-05-02 16:00:02 +0300

    feat: created base  midleware class

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { GoogleAuthGuard } from './utils/Guards';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Get('google/login')
+  @UseGuards(GoogleAuthGuard)
   getAuth(): string {
     return this.authService.getAuthHello();
   }
 
   @Get('google/redirect')
+  @UseGuards(GoogleAuthGuard)
   getUserRedirect() {
     return 'Google redirect';
   }
--- src/auth/utils/GoogleStrategy.ts
+++ src/auth/utils/GoogleStrategy.ts
@@ -1,13 +1,21 @@
+import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy } from 'passport-google-oauth20';
+import { Profile, Strategy } from 'passport-google-oauth20';
 
+@Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: `http://localhost:${process.env.PORT}/auth/google/redirect`,
-      scope: [],
+      scope: ['profile', 'email'],
     });
+  }
+
+  async validate(accessToken: string, refreshToken: string, profile: Profile) {
+    console.log(accessToken);
+    console.log(refreshToken);
+    console.log(profile);
   }
 }
