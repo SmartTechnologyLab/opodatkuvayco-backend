205c8f3e5f205dd751ca90ce585a3dae7bbac25f
Yevhenii <zheki2004@gmail.com>
2024-05-04 16:32:31 +0300

    feat: added swagger doc for each endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Test endpoint')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiResponse({
+    status: 200,
+    description: 'Test endpoint',
+    schema: {
+      example: 'Hello world',
+    },
+  })
   getHello(): string {
     return this.appService.getHello();
   }
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,25 +1,69 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { GoogleAuthGuard } from './utils/Guards';
 import { Request } from 'express';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('registration')
 @Controller('auth')
 export class AuthController {
   @Get('google/login')
   @UseGuards(GoogleAuthGuard)
-  getAuth() {
+  @ApiResponse({
+    status: 200,
+    description: 'Successfully initiated Google authentication',
+    schema: {
+      properties: {
+        message: { type: 'string', example: 'Google Auth' },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Failed to initiate Google authentication',
+  })
+  getUserAuth() {
     return { message: 'Google Auth' };
   }
 
   @Get('google/redirect')
   @UseGuards(GoogleAuthGuard)
+  @ApiResponse({
+    status: 200,
+    description: 'User succesfully got redirected',
+  })
   getUserRedirect() {
     return { message: 'OK' };
   }
 
-  @Get('status')
+  @Get('userInfo')
+  @ApiResponse({
+    status: 200,
+    description: 'User status retrieved successfully',
+    schema: {
+      properties: {
+        msg: { type: 'string', example: 'Authenticated' },
+        user: {
+          type: 'object',
+          example: { id: '1', displayName: 'testUser', email: 'test@test.com' },
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'User status retrieved unsuccessfully',
+    schema: {
+      properties: {
+        msg: {
+          type: 'string',
+          example: 'Not Authenticated',
+        },
+      },
+    },
+  })
   userStatus(@Req() request: Request) {
     if (request.user) {
-      return { msg: 'Authenticated' };
+      return { msg: 'Authenticated', user: request.user };
     }
 
     return { msg: 'Not Authenticated' };
