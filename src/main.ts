c4c3d6a6bb46a12f877fe3ca35d39e054719797f
ZadorozhnyiYevhenii <yevhenii.zadorozhnyi.dev@gmail.com>
2024-08-17 16:15:41 +0300

    feat: added swagger for report endpoint

--- src/main.ts
+++ src/main.ts
@@ -7,25 +7,37 @@ import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   dotenv.config();
+
   const app = await NestFactory.create(AppModule);
+
   app.use(bodyParser.json({ limit: '50mb' }));
+
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
   app.use(
     cors({
-      origin: 'http://localhost:5173',
+      origin: process.env.CLIENT_URL,
       methods: ['GET', 'POST', 'PUT', 'DELETE'],
       credentials: true,
     }),
   );
+
+  app.setGlobalPrefix('api');
+
   const config = new DocumentBuilder()
     .setTitle('Opodatkuvayco Backend')
     .setVersion('1.0')
     .addTag('REST API')
+    .addBasicAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
 
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
 
   await app.listen(process.env.PORT || 3000);
 }
--- src/report/report.controller.ts
+++ src/report/report.controller.ts
@@ -9,13 +9,41 @@ import { ReportService } from './report.service';
 import { FilesInterceptor } from '@nestjs/platform-express';
 import { Deal, IDealReport } from './types';
 import { StockExchange } from '../normalizeTrades/constants';
+import {
+  ApiBody,
+  ApiOperation,
+  ApiQuery,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('report')
 @Controller('report')
 export class ReportController {
   constructor(private reportService: ReportService) {}
 
   @Post('deals')
   @UseInterceptors(FilesInterceptor('file', 10))
+  @ApiOperation({ summary: 'Getting deals' })
+  @ApiQuery({ name: 'type', description: 'Report type', required: false })
+  @ApiBody({
+    description: 'Loading trades report in JSON format for getting deals',
+    required: true,
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'string',
+          format: 'binary',
+          description: 'JSON file containing trades report',
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Returns deals',
+  })
   async getDeals(
     @Query('type') type: string,
     @UploadedFiles() files: Express.Multer.File[],
