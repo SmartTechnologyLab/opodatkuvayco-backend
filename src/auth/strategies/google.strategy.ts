6785c3dde0102cccae8f6da11d5e9ce8a4d3760c
ZadorozhnyiYevhenii <yevhenii.zadorozhnyi.dev@gmail.com>
2025-02-08 18:29:08 +0200

    feat: add env var for callbackUrl

--- src/auth/strategies/google.strategy.ts
+++ src/auth/strategies/google.strategy.ts
@@ -9,7 +9,7 @@ export class GoogleOAuthStrategy extends PassportStrategy(Strategy, 'google') {
     super({
       clientID: process.env.GOOGLE_AUTH_CLIENT_ID,
       clientSecret: process.env.GOOGLE_AUTH_CLIENT_SECRET,
-      callbackURL: 'http://localhost:3000/auth/google/redirect',
+      callbackURL: process.env.GOOGLE_AUTH_REDIRECT_URL,
       scope: ['email', 'profile'],
     });
   }
