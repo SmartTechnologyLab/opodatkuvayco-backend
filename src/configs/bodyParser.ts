768bbb1769c4e265eba061453efb74ce8be10241
Vitaly <poter925@gmail.com>
2025-04-21 13:47:35 +0300

    Revert "refactor: move main configs to config function"
    This reverts commit f04d1fce84c05478b7940fe50fbd22f1cd04ba19.

--- src/configs/bodyParser.ts
+++
@@ -1,9 +0,0 @@
-import { INestApplication } from '@nestjs/common';
-import * as bodyParser from 'body-parser';
-
-export const bodyParserConfig = (app: INestApplication) => {
-  app.use(bodyParser.json({ limit: '50mb' }));
-  app.use(bodyParser.text({ type: 'application/xml', limit: '50mb' }));
-
-  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-};
