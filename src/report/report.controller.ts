253e7aded86d4f2090cfa70bbbd83b0bd63647d0
Vitaly <poter925@gmail.com>
2025-04-21 13:47:35 +0300

    Revert "feat: added demo report endpoint"
    This reverts commit ee45a74a4e30da929bbdd37dcdd6f2f9b9a726b9.

--- src/report/report.controller.ts
+++ src/report/report.controller.ts
@@ -6,7 +6,6 @@ import {
   Query,
   Req,
   Request,
-  UploadedFile,
   UploadedFiles,
   UseGuards,
   UseInterceptors,
@@ -14,7 +13,7 @@ import {
   ValidationPipe,
 } from '@nestjs/common';
 import { ReportService } from './report.service';
-import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
+import { FilesInterceptor } from '@nestjs/platform-express';
 import { ApiBody, ApiConsumes, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { JwtGuard } from '../auth/guards/jwt.quard';
 import { Report } from './entities/report.entity';
@@ -85,52 +84,6 @@ export class ReportController {
       files,
       user: req.user,
       stockExchange: query.stockExchange,
-    });
-  }
-
-  @Post('create-demo-report')
-  @ApiConsumes('multipart/form-data')
-  @UseInterceptors(
-    FileInterceptor('file', {
-      fileFilter: (req, file, callback) => {
-        if (
-          file.mimetype === 'application/json' ||
-          file.mimetype === 'application/xml'
-        ) {
-          callback(null, true);
-        } else {
-          callback(
-            new BadRequestException(
-              `Invalid file type: ${file.mimetype}. Only JSON and XML files are allowed.`,
-            ),
-            false,
-          );
-        }
-      },
-    }),
-  )
-  @UsePipes(new ValidationPipe({ transform: true }))
-  @ApiBody({
-    description: 'Upload JSON or XML file containing trades report',
-    required: true,
-    schema: {
-      type: 'object',
-      properties: {
-        file: {
-          type: 'string',
-          format: 'binary',
-          description: 'JSON or XML file containing trades report',
-        },
-      },
-    },
-  })
-  async processDemoFile(
-    @UploadedFile() file: Express.Multer.File,
-    @Query() query: ReportDealsDto,
-  ) {
-    return await this.reportService.processDemoReport({
-      file,
-      stockExhange: query.stockExchange,
     });
   }
 }
