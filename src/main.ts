ff18302fa578ca11baa3d74c3b55b09472f10ce3
Vitaly <poter925@gmail.com>
2025-04-21 13:47:35 +0300

    Revert "refactor: move main configs to config function"
    This reverts commit f04d1fce84c05478b7940fe50fbd22f1cd04ba19.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as cors from 'cors';
+import * as bodyParser from 'body-parser';
 import * as dotenv from 'dotenv';
 import { ConfigService } from '@nestjs/config';
-import { appConfig } from './configs';
+import { ValidationPipe } from '@nestjs/common';
+import * as basicAuth from 'express-basic-auth';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,11 +14,50 @@ async function bootstrap() {
 
   dotenv.config();
 
+  // any property not included in the whitelist is automatically stripped from the resulting object
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  // Get allowed origins from environment variables
+  const allowedOrigins = configService.get<string>('CLIENT_URL');
   // Get host and port from environment variables
   const host = configService.get<string>('HOST') || '0.0.0.0';
   const port = parseInt(configService.get<string>('PORT'), 10) || 3000;
 
-  appConfig(app, configService);
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.text({ type: 'application/xml', limit: '50mb' }));
+
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  app.use(
+    cors({
+      origin: allowedOrigins,
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      credentials: true,
+    }),
+  );
+
+  app.use(
+    '/docs*',
+    basicAuth({
+      authorizer: (username: string, password: string) =>
+        username === configService.get('SWAGGER_HTTP_BASIC_AUTH_USERNAME') &&
+        password === configService.get('SWAGGER_HTTP_BASIC_AUTH_PASSWORD'),
+      challenge: true,
+    }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('Opodatkuvayco Backend')
